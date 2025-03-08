013dec07e0f52f39bca9558adbfd2921e5e5d8d4
Ahmad_jafari19 <146614541+ahmadreza19@users.noreply.github.com>
2025-03-08 10:57:02 +0330

    Prevent CSRF or XSRF attacks
    - When this property is set on a controller method, it checks that requests sent to that method contain a valid Anti-Forgery token.
    - This token must be created using Html.AntiForgeryToken()@ in the corresponding form.
    - If the token does not exist or is invalid, the request is rejected.

--- TechBack_security/TechBack_security/Controllers/HomeController.cs
+++ TechBack_security/TechBack_security/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+﻿using System;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using TechBack_security.Models;
 
@@ -7,18 +9,28 @@ namespace TechBack_security.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private  IConfiguration _configuration;
+        private IConfiguration _configuration;
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
         }
+        //[ValidateAntiForgeryToken]
+        //این یک ویژگی(attribute) در ASP.NET است که برای محافظت از برنامه‌های وب در برابر حملات
+        //Cross-Site Request Forgery (CSRF یا XSRF) استفاده می‌شود.
 
+        //عملکرد:
+        //- این ویژگی زمانی که روی یک متد کنترلر قرار می‌گیرد، بررسی می‌کند که درخواست‌های ارسالی
+        // به آن متد حاوی یک توکن معتبر Anti-Forgery باشند
+        //- این توکن باید با هلپر @Html.AntiForgeryToken() در فرم مربوطه ایجاد شده باشد
+        //- اگر توکن وجود نداشته باشد یا نامعتبر باشد، درخواست رد می‌شود
+
+        [ValidateAntiForgeryToken]
         public string Index()
         {
             return _configuration["password"].ToString();
         }
-
+        [ValidateAntiForgeryToken]
         public IActionResult Privacy()
         {
             return View();
